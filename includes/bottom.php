884c80a8c9afe51d0918e9307cbb81b53cae11f3
thorney29 <tresa.e@gmail.com>
2017-02-03 13:31:13 -0800

    removed forward slash to js files in bottom.php

--- includes/bottom.php
+++ includes/bottom.php
@@ -10,11 +10,11 @@
 <!-- <script src="scripts/bjqs-1.3.min.js"></script> 
 <script src="scripts/jquery.ba-bbq.min.js"></script> 
 <script src="scripts/slider.js"></script>-->
-<script src="/scripts/jquery.qtip.min.js"></script>
-<script src="/scripts/rAF.js"></script>
-<script src="/scripts/demo-2.js"></script>
+<script src="scripts/jquery.qtip.min.js"></script>
+<script src="scripts/rAF.js"></script>
+<script src="scripts/demo-2.js"></script>
 
-<script src="/scripts/scripts3.js"></script>
+<script src="scripts/scripts3.js"></script>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
