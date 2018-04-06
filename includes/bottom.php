e38eeb52024ab4859228842b9f160b495f80e3e2
thorney29 <tresa.e@gmail.com>
2018-04-06 12:56:42 -0700

    update services and footer file

--- includes/bottom.php
+++ includes/bottom.php
@@ -5,17 +5,25 @@
       <li><a href="https://twitter.com/tresahorney" title="Follow me on Twitter"><i class="fa fa-twitter-square" aria-hidden="true"></i>
 </a></li>
   </ul>
-  <p>This site is maintained by: <a href="mailto:tresa.e@gmail.com">Tresa Horney</a> </p>
-</footer>
-<!-- <script src="scripts/bjqs-1.3.min.js"></script> 
-<script src="scripts/jquery.ba-bbq.min.js"></script> 
-<script src="scripts/slider.js"></script>-->
-<script src="scripts/jquery.qtip.min.js"></script>
-<script src="scripts/rAF.js"></script>
-<script src="scripts/demo-2.js"></script>
+  <p>Have questions? <a href="mailto:tresa@tresahorney.com">Say Hello.</a><br />
+  Need to pay your invoice? <a href="/make-a-payment">Make a payment now.</a> </p>
+  <a href="/"><img src="../img/tresa-horney-logo-updated-green-v9.png" alt="tresa horney site logo"></a>
 
-<script src="scripts/scripts3.js"></script>
-<script>
+</footer>
+
+
+<script async src="/scripts/modernizr.custom.41979.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  --><!-- <script type="text/javascript">
+$(document).ready(function(){
+    $('html').css('visibility', 'visible');
+  })
+</script> -->
+<script type="text/javascript" src="/scripts/jquery.qtip.min.js"></script>
+<script type="text/javascript" src="/scripts/demo-2.js"></script>
+
+<script async type="text/javascript" src="/scripts/scripts3_compressed.js"></script>
+<!-- <script type="text/javascript">
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -24,6 +32,19 @@
   ga('create', 'UA-87031845-1', 'auto');
   ga('send', 'pageview');
 
+</script> -->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106747365-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-106747365-1');
+  $("#large-header").css('visibility',"visible");
+
 </script>
+
  </body>
 </html>
--- services.php
+++ services.php
@@ -7,32 +7,54 @@ include("includes/top.php");
  <?php 
 include("includes/middle.php"); 
 ?>
-		<article>
+		 <article class="services">
             <h1>Services</h1>
-            <h3>Need a New Website</h3>
-            <p>Have you ever asked, "Do I really need a website?" 
-            If you're looking to build trust with your audience, connect with new customers, or take your business to the next level, you do. 
-            </p>
-            <p><strong>Like a brochure but better:  </strong>
-           Websites can introduce services and offerings to prospective customers in stress-free settings at a time that's convenient for the consumer. Having an online presence helps to build brand awareness, trust and open communication with new and returning customers. </p>
-           <p>Rate: Hourly or project based </p>
+            <div class="heading-3">
+              <h3><a href=""><span>Need a New Website?</span></a></h3>
+            </div>
+            <hr class="brace">
+            <div class="flexbox">
+              <div class="service-icons"><i class="fa fa-desktop" aria-hidden="true"></i></div>
+              <div>
+              <p>Are you looking to build trust with your audience, connect with new customers, or take your business to the next level? It's time to build a website.</p>
+              <p>
+              Websites can introduce services and offerings to prospective customers in stress-free settings at a time that's convenient for them. A professional online presence helps to build brand awareness, trust and open communication with new and returning customers. </p>
+              </div>
+            </div>
+            <div class="heading-3">
+              <h3><a href=""><span>Need a Redesign or Refresh?</span></a></h3>
+            </div>
+            <hr class="brace">
+            <div class="flexbox">
+              <div class="service-icons"><i class="fa fa-refresh bluish" aria-hidden="true"></i></div>
+              <div><p>As time goes by, styles change, and not just in fashion. The most well-designed websites can look dated after only a few years. If your site is no longer the site of your dreams, it may time to consider a redesign or site refresh. I can help you restore your site to its former glory with a site design update. </p>
+              </div>
+            </div>
+            <div class="heading-3">
+              <h3><a href=""><span>Need Updates & Maintenance?</span></a></h3>
+            </div> 
+            <hr class="brace">
+            <div class="flexbox">
+              <div class="service-icons"><i class="fa fa-stethoscope" aria-hidden="true"></i></div>
+              <div>
+              <p>Do you have a great website, but not the time or skills to keep it current or troubleshoot for bugs?</p>
+              <p>It can be exciting to build a website for your business; to finally see your products available in your online store or your organization's upcoming events posted on your online calendar. But what happens after the shine wears off and you're too busy to keep up? 
 
-            <h3>Website Refresh</h3>
-            <p>You have a website, but it no longer looks like the site of your dreams (and it's only been a year since you had it built). Not to worry! I can help you restore you site to it's former glory with a site design update. </p>
-            <p>Rate: Hourly or project based</p>
+              <a href="/contact">Contact me now</a> to find out how I can help you keep your site up-to-date.</p>
+              </div>
+            </div>  
+             <hr style="background-color: #ccc;margin: 20px 0">
 
-            <h3>Site Updates & Maintenance Plan</h3>
-            <p>What happens when you have a website, but you don't have the time or the coding skills to update and maintain it? 
-	 		</p>
-            <p>
-            It can be exciting to build a website for your business; to finally see your products available in your newly created online store or your organization's upcoming events posted on your new online calendar. But what happens after the shine wears off and you're too busy to update the products, add a new blog post or update the event calendar? 
+            <h3>My Rates</h3>
+            <p><strong>Rates are negotiated based on project size and scope. <em>Nonprofits receive a 30% discount.</em></strong></p>
+            <h3>My Preferred Host Provider</h3>
+            <a href="/company#environment" title="Green Hosting powered by Rochen" target="blank">
+              <img class="rochen" src="//cdn.rochen.com/images/rochen-green-hosting.png" alt="Green Hosting">
+            </a> 
+            <p>A beautiful website cannot reach its full potential without a great host. That's why I use and recommend Rochen to my clients. Rochen offers reliable hosting services, provides outstanding customer service, and understands their impact on the environment. They work with Green Mountain Energy to offset their carbon emissions with carbon credits. <a href="https://my.rochen.com/?link=7935" target="_blank">Learn more about Rochen.</a>
+          
 
-            I can help keep your site up-to-date whether you use Wordpress, Drupal or other.</p>
-            <p>Rate: 85/hour </p>
-
-            <h3>Rates</h3>
-            <p>Rates are negotiated based on project size and scope. Hourly rates start at $85/hour. Nonprofits receive a 25% discount.</p>
-              </article>
+        </article>
 <?php 
 include("includes/bottom.php"); 
 ?>
